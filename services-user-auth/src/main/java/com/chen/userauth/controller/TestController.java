7c1d90dec9b4b8555a3f466ab58b301e192c596b
huang.zhaochen <a675750333@gmail.com>
2021-05-28 14:02:03 +0800

    refactor(账号服务): 移除无用代码

--- services-user-auth/src/main/java/com/chen/userauth/controller/TestController.java
+++ services-user-auth/src/main/java/com/chen/userauth/controller/TestController.java
@@ -2,17 +2,15 @@ package com.chen.userauth.controller;
 
 import com.chen.userauth.entity.AuthUser;
 import com.chen.userauth.entity.dto.UserDto;
+import com.chen.userauth.service.JwtService;
 import com.chen.userauth.service.UserAuthService;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.security.KeyPair;
-import java.security.interfaces.RSAPublicKey;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author chen
@@ -29,18 +27,18 @@ public class TestController {
     private HttpServletRequest request;
 
     @GetMapping("/user")
-    public List<AuthUser> getAllUser(){
+    public List<AuthUser> getAllUser() {
         return userAuthService.getAllUser();
     }
 
     @GetMapping("/user/{username}")
-    public ResponseEntity<AuthUser>  getUser(@PathVariable("username") String username){
-        Optional<AuthUser> authUser=Optional.ofNullable(userAuthService.getUserByUsername(username));
+    public ResponseEntity<AuthUser> getUser(@PathVariable("username") String username) {
+        Optional<AuthUser> authUser = Optional.ofNullable(userAuthService.getUserByUsername(username));
         return ResponseEntity.of(authUser);
     }
 
     @PostMapping("/user")
-    public void createUser(@RequestBody UserDto user){
+    public void createUser(@RequestBody UserDto user) {
         userAuthService.createUser(user);
     }
 
