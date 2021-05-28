0c44328d8b9beffe7f53bb8968cec3f56efb3fea
huang.zhaochen <a675750333@gmail.com>
2021-05-28 15:14:13 +0800

    refactor(公共模块): Redis服务抽出，放入公共模块

--- services-user-auth/src/main/java/com/chen/userauth/UserAuthApplication.java
+++ services-user-auth/src/main/java/com/chen/userauth/UserAuthApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @author chen
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.chen.userauth", "com.chen.common"})
 @EnableDiscoveryClient
 @EnableCaching
 @MapperScan("com.chen.userauth.mapper")
